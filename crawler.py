0c9b7418a0b3248d945414e686c5b27a974e4b0a
Walter Heck <walterh@helecloud.com>
2024-03-19 23:22:45 +0100

    Updates the crawler process
    * split off some of the code to a separate file
    * consolidate all credentials needed in settings.json
    * implements a workaround for credentials handling by langchain's GoogleDriveLoader

--- crawler.py
+++ crawler.py
@@ -4,19 +4,25 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 import sqlite3
 import json
+import os
+from pathlib import Path
+import lorelai_pinecone
+import tempfile
 
 # The scopes needed to read documents in Google Drive
 SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 DATABASE = './userdb.sqlite'
 
-# sqlite> PRAGMA table_info(user_tokens)
-#    ...> ;
-# 0|user_id|TEXT|0||1
-# 1|access_token|TEXT|0||0
-# 2|refresh_token|TEXT|0||0
-# 3|expires_in|INTEGER|0||0
-# 4|token_type|TEXT|0||0
-# 5|scope|TEXT|0||0
+# load openai creds from file
+def load_openai_creds():
+    with open('settings.json') as f:
+        return json.load(f)['openai']
+
+# load pinecone creds from file 
+def load_pinecone_creds():
+    with open('settings.json') as f:
+        return json.load(f)['pinecone']
+
 # load tokens from sqlite
 def load_tokens_from_sqlite():
     conn = sqlite3.connect(DATABASE)
@@ -26,38 +32,52 @@ def load_tokens_from_sqlite():
     conn.close()
     return rows 
 
-def load_client_secrets():
-    with open('client_secret.json') as f:
-        return json.load(f)
-    
-def main():
-    creds = None
-    secrets = load_client_secrets()
-    
+def load_google_creds_to_tempfile():
+  
+    with open('settings.json') as f:
+        secrets = json.load(f)['google']
+
     tokens = load_tokens_from_sqlite()
+
     if tokens:
         token = tokens[0]
-        creds = Credentials(
-            token[1],
-            refresh_token=token[2],
-            token_uri="https://oauth2.googleapis.com/token",
-            client_id=secrets['web']['client_id'],
-            client_secret=secrets['web']['client_secret']
-        )
-
-    service = build('drive', 'v3', credentials=creds)
-
-    # Call the Drive v3 API to list the documents in the root folder
-    results = service.files().list(
-        pageSize=10, fields="nextPageToken, files(id, name)", q="'root' in parents and mimeType='application/vnd.google-apps.document'").execute()
-    items = results.get('files', [])
-
-    if not items:
-        print('No documents found.')
+        refresh_token = token[2]
+        token_uri = "https://oauth2.googleapis.com/token"
+        client_id = secrets['client_id']
+        client_secret = secrets['client_secret']
+        
+        print(f"Token Info: {token} Refresh Token: {refresh_token} Token URI: {token_uri} Client ID: {client_id} Client Secret: {client_secret}")
+        
+        # create a file: Path.home() / ".credentials" / "token.json" to store the credentials so they can be loaded by GoogleDriveLoader's auth process (this uses Credentials.from_authorized_user_file)        
+        if not os.path.exists(Path.home() / ".credentials"):
+            os.makedirs(Path.home() / ".credentials")
+            
+        with open(Path.home() / ".credentials" / "token.json", 'w') as f:
+            f.write(json.dumps({
+                "refresh_token": refresh_token,
+                "token_uri": token_uri,
+                "client_id": client_id,
+                "client_secret": client_secret
+            }))
+            f.close()
     else:
-        print('Documents:')
-        for item in items:
-            print(u'{0} ({1})'.format(item['name'], item['id']))
+        print("No tokens found in sqlite")
+
+    
+def main():
+    load_google_creds_to_tempfile()
+    
+    pinecone_creds = load_pinecone_creds()
+    
+    openai_creds = load_openai_creds()
+        
+    # Make sure to replace 'your_pinecone_api_key', 'your_environment', and 'your_index_name'
+    # with your actual Pinecone API key, environment, and index name.
+    processor = lorelai_pinecone.GoogleDriveProcessor(pinecone_api_key=pinecone_creds['api-key'], 
+                                                      pinecone_environment=pinecone_creds['environment'], 
+                                                      pinecone_index_name=pinecone_creds['index-name'],
+                                                      openai_api_key=openai_creds['api-key'])
+    processor.processDrive()
 
 if __name__ == '__main__':
     main()
