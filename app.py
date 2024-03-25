93209f96db7f69ad4582e86a08c3451b8131f20f
Walter Heck <walterh@helecloud.com>
2024-03-25 15:51:55 +0100

    introduce organisations
    automatically derive an organisation for now from the domain of the google user signing up. Also changes the sqlite schema to adjust for this.

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ from google.oauth2 import id_token
 from google_auth_oauthlib.flow import Flow
 import google.auth.transport.requests
 
+def get_db_connection():
+    """returns a connection to the database
+
+    Returns:
+        conn: the connection to the database
+    """
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
+
 app = Flask(__name__)
 app.secret_key = 'your_very_secret_and_long_random_string_here'
 
@@ -41,15 +51,32 @@ flow = Flow.from_client_config(
 # Database setup
 DATABASE = './userdb.sqlite'
 
-def get_db_connection():
-    """returns a connection to the database
+# Database schema
+connection = get_db_connection()
+cur = connection.cursor()
 
-    Returns:
-        conn: the connection to the database
-    """
-    conn = sqlite3.connect(DATABASE)
-    conn.row_factory = sqlite3.Row
-    return conn
+# make sure the organisation table exists
+cur.execute('''CREATE TABLE IF NOT EXISTS organisations (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE
+)''')
+
+# make sure the users table exists
+cur.execute('''CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    org_id INTEGER,
+    name TEXT,
+    email TEXT,
+    access_token TEXT,
+    refresh_token TEXT,
+    expires_in INTEGER,
+    token_type TEXT,
+    scope TEXT
+)''')
+
+connection.commit()
+cur.close()
+connection.close()
 
 # shows a link to lorelai if the user is logged in, otherwise shows a link to login
 @app.route('/')
@@ -94,53 +121,54 @@ def callback():
 
     # Here, 'sub' is used as the user ID. Depending on your application, you might use a different
     # identifier
-    user_id = id_info.get('sub')
+    # user_id = id_info.get('sub')
     username = id_info.get('name')
     user_email = id_info.get('email')
+    organisation = id_info.get('email').split('@')[1]
 
     # Database insert/update
     conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('''CREATE TABLE IF NOT EXISTS users (
-        id TEXT PRIMARY KEY,
-        name TEXT,
-        email TEXT
-    )''')
-    cur.execute('''CREATE TABLE IF NOT EXISTS user_tokens (
-        user_id TEXT PRIMARY KEY,
-        access_token TEXT,
-        refresh_token TEXT,
-        expires_in INTEGER,
-        token_type TEXT,
-        scope TEXT
-    )''')
+    cursor = conn.cursor()
 
-    cur.execute(("""
-        INSERT INTO users (id, name, email)
-        VALUES (?, ?, ?)
-        ON CONFLICT (id)
-        DO UPDATE SET email = EXCLUDED.email, name = EXCLUDED.name;
-    """), (user_id, username, user_email))
+    print(f"ORG: {organisation}")
 
-    cur.execute('''
-        INSERT INTO user_tokens (user_id, access_token, refresh_token, expires_in, token_type, scope)
-        VALUES (?, ?, ?, ?, ?, ?)
-        ON CONFLICT(user_id) DO UPDATE SET
-        access_token=excluded.access_token,
-        refresh_token=excluded.refresh_token,
-        expires_in=excluded.expires_in,
-        token_type=excluded.token_type,
-        scope=excluded.scope;
-        ''', (
-        user_id,
+    # create a new organisation if it doesn't exist
+    # if it does exist, return it's id
+    cursor.execute(("""
+        INSERT INTO organisations (name)
+        VALUES (?)
+        ON CONFLICT (name)
+        DO NOTHING;
+    """), (organisation,))
+    conn.commit()
+
+    org_id = cursor.execute("SELECT id FROM organisations WHERE name = ?;", (organisation,)).fetchone()[0]
+    conn.commit()
+
+    cursor.execute(("""
+        INSERT INTO users (
+            org_id, 
+            name,
+            email,
+            access_token,
+            refresh_token,
+            expires_in,
+            token_type,
+            scope)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?);
+    """), (
+        org_id,
+        username,
+        user_email,
         credentials.token,
         credentials.refresh_token,
         credentials.expiry,
         "Bearer",
-        ' '.join(credentials.scopes)
+        ' '.join(str(credentials.scopes))
     ))
+
     conn.commit()
-    cur.close()
+    cursor.close()
     conn.close()
 
     session['google_id'] = id_info.get('sub')
