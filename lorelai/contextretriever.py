b892048b1540083fb83c55e493c8bff419c6c5ab
Walter Heck <walterh@helecloud.com>
2024-03-26 13:59:54 +0100

    Renamed test_query.py to lorelaicli.py
    * renamed the script for clarity
    * split off lorelai functionality to the contextretriever class
    * the new cli is aware of users and orgs, and has basic prep for datasources

---
+++ lorelai/contextretriever.py
@@ -0,0 +1,81 @@
+"""This module is used to retrieve the context for a question from pinecone
+"""
+
+import json
+import os
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from lorelai.utils import pinecone_index_name
+
+class Contextretriever:
+    """this class is used to retrieve the context for a question from pinecone
+    """
+    # load openai creds from file and set env variable
+    @staticmethod
+    def load_openai_creds():
+        """loads the openai creds from the settings.json file
+        """
+        with open('settings.json', encoding='utf-8') as f:
+            creds = json.load(f)['openai']
+        os.environ["OPENAI_API_KEY"] = creds['api-key']
+        return creds
+
+    @staticmethod
+    def load_pinecone_creds():
+        """loads the pinecone creds from the settings.json file
+        """
+        with open('settings.json', encoding='utf-8') as f:
+            creds = json.load(f)['pinecone']
+        os.environ["PINECONE_API_KEY"] = creds['api-key']
+        return creds
+
+
+    def __init__(self, org_name, user):
+        """initializes the class and loads the pinecone and openai creds
+        """
+        pinecone_creds = self.load_pinecone_creds()
+        openai_creds = self.load_openai_creds()
+
+        self.pinecone_creds = pinecone_creds
+        self.openai_creds = openai_creds
+
+        self.pinecone_api_key = pinecone_creds['api-key']
+        self.openai_api_key = openai_creds['api-key']
+
+        os.environ["OPENAI_API_KEY"] = self.openai_api_key
+        os.environ["PINECONE_API_KEY"] = self.pinecone_api_key
+
+        self.org_name = org_name
+        self.user = user
+
+    def retrieve_context(self, question):
+        """retrieves the context for the question
+        """
+
+        Template = """Beantwoord de volgende vraag alleen gebaseerd op de context hieronder:
+        {context}
+
+        Vraag: {question}
+        """
+        prompt = PromptTemplate.from_template(Template)
+
+        model = ChatOpenAI(model="gpt-3.5-turbo")
+        output_parser = StrOutputParser()
+        
+        
+        index_name = pinecone_index_name(self.org_name, "googledrive")
+        vector_store = PineconeVectorStore(index_name=index_name, embedding=OpenAIEmbeddings())
+
+        retriever = vector_store.as_retriever()
+
+        docs = retriever.get_relevant_documents(question, k=1)
+
+        chain = prompt | model | output_parser
+
+        result = chain.invoke({"context": docs, "question": question})
+
+        return result
---
+++ lorelaicli.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+"""this script is used to query the indexed documents in pinecone using langchain and OpenAI
+"""
+
+import sqlite3
+import sys
+
+from lorelai.contextretriever import Contextretriever
+
+# a simple test script to ask a question langchain that has been indexed in pinecone
+
+# the question to ask is supplied as commenad line argument
+args = sys.argv
+question = args[1]
+
+def select_organisation():
+    """ list the organisations in sqlite and ask the user to pick one, defaulting to the first one
+    """
+    sql = "SELECT id, name FROM organisations"
+    conn = sqlite3.connect('userdb.sqlite')
+    cur = conn.cursor()
+    orgs = cur.execute(sql).fetchall()
+
+    print("Select an organisation:")
+    # start numbering at 1
+    for i, org in enumerate(orgs):
+        print(f"{i+1}: {org[1]}")
+
+    choice = input(f"Organisation ({orgs[0][1]}): ")
+
+    # if no id is given, default to the first one
+    if choice:
+        org = orgs[int(choice)-1][1]
+    else:
+        org = orgs[0][1]
+        choice = orgs[0][0]
+
+    return org, choice
+
+def select_user_from_organisation(organisation_id):
+    """ list the users in sqlite and ask the user to pick one, defaulting to the first one
+    """
+    sql = f"SELECT user_id, name, email FROM users WHERE org_id = {organisation_id}"
+    conn = sqlite3.connect('userdb.sqlite')
+    cur = conn.cursor()
+    users = cur.execute(sql).fetchall()
+
+    print("Select a user:")
+    # start numbering at 1
+    for i, user in enumerate(users):
+        print(f"{i+1}: {user[1]} ({user[2]})")
+
+    choice = input(f"User ({users[0][1]}): ")
+
+    # if no id is given, default to the first one
+    if not choice:
+        choice = users[0][0]
+
+    return choice
+
+org_name, org_id = select_organisation()
+
+user_id = select_user_from_organisation(org_id)
+
+
+# get the context for the question
+enriched_context = Contextretriever(org_name=org_name, user=user_id)
+
+answer = enriched_context.retrieve_context(question)
+
+print(answer)
