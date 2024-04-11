dbd6088c3aa942eddb9ba48b15428af2d5397014
Masood-Helix <165290860+Masood-Helix@users.noreply.github.com>
2024-04-11 20:00:28 +0530

    86by3y0cv update metadata tag for 2nd user in db (#7)
    Corrected an issue with the refresh token.
    Implemented a feature that tags an existing document in the database with a new user if they have access rights. Additionally, introduced deduplication.
    Performed text cleaning for database optimization.
    Eliminated a redundant comment.
    Addressed a typographical error.
    Enhanced error handling to alert mismatches in embeds.
    Conducted code quality improvements using Pylint.
    Applied code formatting with the Black formatter as the default setting.
    Fixed a spelling mistake.
    Improved linting by replacing range and len with enumerate for better efficiency.
    Carried out further linting enhancements.

--- lorelai/processor.py
+++ lorelai/processor.py
@@ -1,38 +1,125 @@
 """This module contains the Processor class that processes documents and indexes them in Pinecone
 """
+
 import os
+import uuid
 from typing import Iterable, List
 
 import pinecone
 from google.oauth2.credentials import Credentials
+import pinecone
+from pinecone import ServerlessSpec
 from langchain_community.document_loaders.googledrive import GoogleDriveLoader
 from langchain_core.documents import Document
 from langchain_openai import OpenAIEmbeddings
-from langchain_pinecone import PineconeVectorStore
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-from pinecone import ServerlessSpec
-
-from lorelai.utils import (get_embedding_dimension, load_config, pinecone_index_name,
-                           save_google_creds_to_tempfile)
+from lorelai.utils import pinecone_index_name, load_creds, save_google_creds_to_tempfile
+from lorelai.utils import get_embedding_dimension
 
 
 class Processor:
-    """This class is used to process the Google Drive documents and index them in Pinecone
-    """
+    """This class is used to process the Google Drive documents and index them in Pinecone"""
 
     def __init__(self):
-        """initializes the Processor class
-        """
+        """initializes the Processor class"""
 
         self.pinecone_creds = load_config('pinecone')
         self.openai_creds = load_config('openai')
         self.lorelai_settings = load_config('lorelai')
 
-        self.pinecone_api_key = self.pinecone_creds['api-key']
-        self.openai_api_key = self.openai_creds['api-key']
+        self.pinecone_api_key = self.pinecone_creds["api-key"]
+        self.openai_api_key = self.openai_creds["api-key"]
         # set env variable with openai api key
         os.environ["OPENAI_API_KEY"] = self.openai_api_key
         os.environ["PINECONE_API_KEY"] = self.pinecone_api_key
+
+    def pinecone_filter_deduplicate_documents_list(
+        self, documents: Iterable[Document], pc_index
+    ) -> list:
+        """process the vectors and removes vector which exist in database.
+            Also tag doc metadata with new user
+
+        :param documents: the documents to process
+        :param pc_index: pinecone index object
+
+        :return:1. (list of documents deduplicated and filtered , ready to be inserted in pinecone)
+                2. (number of documents updated)
+        """
+        updated_documents_numbers = 0
+        # Check if docs exist in pinecone.
+        for doc in documents[:]:
+            # doc["metadata"]["users"]=["newuser.com"]
+            result = pc_index.query(
+                vector = doc["values"],
+                top_k = 1,
+                include_metadata = True,
+                filter = {"source": doc["metadata"]["source"]},
+            )
+            # Check if we got matches from query result
+            if len(result["matches"]) > 0:
+                # Check if the vector is already in the database
+                if (
+                    result["matches"][0]["score"] >= 0.99
+                    and result["matches"][0]["metadata"]["source"]
+                    == doc["metadata"]["source"]
+                ):
+
+                    # Check if doc already tag for this users
+                    if (
+                        doc["metadata"]["users"][0]
+                        in result["matches"][0]["metadata"]["users"]
+                    ):
+                        # if so then we remove doc form the document list
+                        documents.remove(doc)
+
+                    # if doc is not tagged for user, then we update the meta data
+                    # to include this user and we remove the doc.
+                    else:
+                        users_list = (
+                            result["matches"][0]["metadata"]["users"]
+                            + doc["metadata"]["users"]
+                        )
+                        pc_index.update(
+                            id=result["matches"][0]["id"],
+                            set_metadata={"users": users_list},
+                        )
+                        documents.remove(doc)
+                        updated_documents_numbers += 1
+
+        return documents, updated_documents_numbers
+
+    def pinecone_format_vectors(
+        self, documents: Iterable[Document], embeddings_model
+    ) -> list:
+        """process the documents and format them for pinecone insert.
+
+        :param docs: the documents to process
+        :param embeddings_model: embeddings_model object
+
+        :return: list of documents ready to be inserted in pinecone
+        """
+        # Get Text
+        text_docs = []
+        for doc in documents:
+            texts = doc.page_content.replace("\n", " ").replace("\r", " ")
+            text_docs.append(texts)
+        embeds = embeddings_model.embed_documents(text_docs)
+        # prepare pinecone vectors
+        formatted_documents = []
+        print(len(documents), len(embeds))
+        if len(documents) != len(embeds):
+            raise ValueError("Embeds length and document length mismatch")
+
+        for i in enumerate(documents):
+            temp_dict = {
+                "id": str(uuid.uuid4()),
+                "values": embeds[i],
+                "metadata": documents[i].metadata,
+            }
+
+            temp_dict["metadata"]["text"] = text_docs[i]
+            formatted_documents.append(temp_dict)
+
+        return formatted_documents
 
     def store_docs_in_pinecone(self, docs: Iterable[Document], index_name) -> None:
         """process the documents and index them in Pinecone
@@ -48,11 +135,13 @@ class Processor:
         documents = splitter.split_documents(docs)
 
         # use text-embedding-ada-002
-        embedding_model = 'text-embedding-ada-002'
-        embeddings = OpenAIEmbeddings(model=embedding_model)
-        embedding_dimension = get_embedding_dimension(embedding_model)
+        embedding_model_name = "text-embedding-ada-002"
+        embedding_model = OpenAIEmbeddings(model=embedding_model_name)
+        embedding_dimension = get_embedding_dimension(embedding_model_name)
         if embedding_dimension == -1:
-            raise ValueError(f"Could not find embedding dimension for model '{embedding_model}'")
+            raise ValueError(
+                f"Could not find embedding dimension for model '{embedding_model}'"
+            )
 
         pc = pinecone.Pinecone(api_key=self.pinecone_api_key)
 
@@ -60,31 +149,45 @@ class Processor:
         # package directly. Check if the index already exists
         if index_name not in pc.list_indexes().names():
             # Create a new index
-            pc.create_index(name=index_name,
-                            dimension=embedding_dimension,
-                            metric='cosine',
-                            spec=ServerlessSpec(
-                                cloud='aws',
-                                region='us-west-2'
-                            ))
+            pc.create_index(
+                name = index_name,
+                dimension = embedding_dimension,
+                metric = "cosine",
+                spec = ServerlessSpec(cloud = "aws", region = "us-west-2"),
+            )
             print(f"Created new Pinecone index {index_name}")
         else:
             print(f"Pinecone index {index_name} already exists")
 
         print(f"Indexing {len(documents)} documents in Pinecone index {index_name}")
 
-        vector_store = PineconeVectorStore(pinecone_api_key=self.pinecone_api_key,
-                                           index_name=index_name, embedding=embeddings)
+        # TODO: subsequent runs should update, not add/duplicate # pylint: disable=fixme
+        pc_index = pc.Index(index_name)
 
-        #TODO: subsequent runs should update, not add/duplicate # pylint: disable=fixme
-        vector_store.from_documents(documents,
-                                            embeddings,
-                                            index_name=index_name)
+        # Format the document for insertion
+        formatted_documents = self.pinecone_format_vectors(documents, embedding_model)
+        filtered_documents, updated_documents_numbers = (
+            self.pinecone_filter_deduplicate_documents_list(
+                formatted_documents, pc_index
+            )
+        )
 
+        # inserting  the documents
+        if filtered_documents:
+            pc_index.upsert(filtered_documents)
+
+        print(
+            f"Updated {updated_documents_numbers} documents in Pinecone index {index_name}"
+        )
         print(f"Indexed {len(documents)} documents in Pinecone index {index_name}")
 
-    def google_docs_to_pinecone_docs(self, document_ids: List[str], credentials: Credentials,
-                                     org_name: str, user_email: str):
+    def google_docs_to_pinecone_docs(
+        self,
+        document_ids: List[str],
+        credentials: Credentials,
+        org_name: str,
+        user_email: str,
+    ):
         """process the Google Drive documents and divide them into pinecone compatible chunks
 
         :param document_id: the document to process
@@ -96,10 +199,12 @@ class Processor:
         """
         # save the google creds to a tempfile as they are needed by the langchain google drive
         # loader until this issue is fixed: https://github.com/langchain-ai/langchain/issues/15058
-        save_google_creds_to_tempfile(refresh_token=credentials.refresh_token,
-                                           token_uri="https://oauth2.googleapis.com/token",
-                                           client_id=credentials.client_id,
-                                           client_secret=credentials.client_secret)
+        save_google_creds_to_tempfile(
+            refresh_token=credentials.refresh_token,
+            token_uri="https://oauth2.googleapis.com/token",
+            client_id=credentials.client_id,
+            client_secret=credentials.client_secret,
+        )
 
         drive_loader = GoogleDriveLoader(document_ids=document_ids)
 
@@ -120,13 +225,14 @@ class Processor:
                 # ${doc.metadata['users']}")
                 doc.metadata["users"].append(user_email)
 
-        #indexname must consist of lower case alphanumeric characters or '-'"
-        index_name = pinecone_index_name(org=org_name,
-                                         datasource='googledrive',
-                                         environment=self.lorelai_settings['environment'],
-                                         env_name=self.lorelai_settings['environment_slug'],
-                                         version="v1")
-
+        # indexname must consist of lower case alphanumeric characters or '-'"
+        index_name = pinecone_index_name(
+            org=org_name,
+            datasource="googledrive",
+            environment=self.lorelai_settings["environment"],
+            env_name=self.lorelai_settings["environment_slug"],
+            version="v1",
+        )
 
         self.store_docs_in_pinecone(docs, index_name=index_name)
         print(f"Processed {len(docs)} documents for user: {user_email}")
