7081c26db2e460f69ce84a26438eecdf1f896fbb
Walter Heck <walterheck@helixiora.com>
2024-04-14 08:18:55 +0200

    Code quality fixes from ruff

--- app/utils.py
+++ app/utils.py
@@ -1,28 +1,32 @@
 """Utility functions for the application."""
 
+import logging
 import sqlite3
 
 from lorelai.utils import load_config
 
 
 def is_admin(google_id: str) -> bool:
-    """Check if the user is an admin"""
+    """Check if the user is an admin."""
     return google_id != ""  # Assuming all users are admins for now
 
 
 # Helper function for database connections
 def get_db_connection() -> sqlite3.Connection:
-    """Get a database connection
+    """Get a database connection.
 
-    Returns:
+    Returns
+    -------
         conn: a connection to the database
+
     """
     try:
         conn = load_config("db")
 
         conn = sqlite3.connect(conn["db_path"])
         conn.row_factory = sqlite3.Row
+    except sqlite3.Error:
+        logging.exception("Database connection failed")
+        raise
+    else:
         return conn
-    except sqlite3.Error as e:
-        print(f"Database connection failed: {e}")
-        raise e
