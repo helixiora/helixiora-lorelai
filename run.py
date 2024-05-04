1122a3c75474f36b520bb7672e4cb05d14f972a0
Walter Heck <walterheck@helixiora.com>
2024-05-05 00:05:38 +0200

    Remove the code that creates the tables in the database
    We now use flyway to create the initial schema and migrations

--- run.py
+++ run.py
@@ -63,42 +63,6 @@ flow = Flow.from_client_config(
     redirect_uri=lorelaicreds["redirect_uri"],
 )
 
-# Database schema
-connection = get_db_connection()
-if connection:
-    cur = connection.cursor()
-else:
-    raise ConnectionError("Failed to connect to the database.")
-
-# make sure the organisation table exists
-cur.execute(
-    """CREATE TABLE IF NOT EXISTS `organisations` (
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(255) UNIQUE,
-    PRIMARY KEY (`id`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"""
-)
-
-# make sure the users table exists
-cur.execute(
-    """CREATE TABLE IF NOT EXISTS `users` (
-    `user_id` INT NOT NULL AUTO_INCREMENT,
-    `org_id` INT,
-    `name` VARCHAR(255),
-    `email` VARCHAR(255),
-    `access_token` TEXT,
-    `refresh_token` TEXT,
-    `expires_in` INT,
-    `token_type` VARCHAR(255),
-    `scope` TEXT,
-    PRIMARY KEY (`user_id`)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;"""
-)
-
-connection.commit()
-cur.close()
-connection.close()
-
 
 # Improved index route using render_template
 @app.route("/")
