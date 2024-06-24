5a04628695f5e7aa887aca5207182c29fc5e433c
Walter Heck <walterheck@helixiora.com>
2024-06-25 01:09:16 +0200

    Add type hints

--- lorelai/indexer.py
+++ lorelai/indexer.py
@@ -1,6 +1,6 @@
-"""Creates a class to process google drive documents using the google drive api.
+"""Creates a class to process Google Drive documents using the Google Drive API.
 
-chunk them using langchain and then index them in pinecone
+Chunk them using Langchain, and then index them in Pinecone.
 """
 
 import logging
@@ -9,8 +9,6 @@ import sys
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
-
-# langchain_community.vectorstores.pinecone.Pinecone is deprecated
 from googleapiclient.discovery import build
 from rq import job
 
@@ -27,8 +25,19 @@ class Indexer:
     _allowed = False  # Flag to control constructor access
 
     @staticmethod
-    def create(datasource="GoogleDriveIndexer"):
-        """Create instances of derived classes based on the class name."""
+    def create(datasource: str = "GoogleDriveIndexer") -> "Indexer":
+        """Create instances of derived classes based on the class name.
+
+        Arguments
+        ---------
+        datasource: str
+            The name of the derived class to instantiate.
+
+        Returns
+        -------
+        Indexer
+            An instance of the derived class.
+        """
         Indexer._allowed = True
         class_ = globals().get(datasource)
         if class_ is None or not issubclass(class_, Indexer):
@@ -38,7 +47,7 @@ class Indexer:
         Indexer._allowed = False
         return instance
 
-    def __init__(self: None):
+    def __init__(self):
         if not self._allowed:
             raise Exception("This class should be instantiated through a create() factory method.")
 
@@ -47,35 +56,36 @@ class Indexer:
 
         os.environ["PINECONE_API_KEY"] = self.pinecone_creds["api_key"]
 
-    def get_indexer_name(self: None) -> str:
+    def get_indexer_name(self) -> str:
         """Retrieve the name of the indexer."""
         return self.__class__.__name__
 
     def index_org(
-        self: None,
-        org_row: dict[any],
-        user_rows: dict[any],
-        user_auth_rows: dict[any],
-        job: job | None = None,
-    ) -> list[dict]:
-        """Process the organisation, indexing all it's users.
+        self,
+        org_row: dict[str, any],
+        user_rows: list[dict[str, any]],
+        user_auth_rows: list[dict[str, any]],
+        job: job.Job | None = None,
+    ) -> list[dict[str, any]]:
+        """Process the organisation, indexing all its users.
 
         Arguments
         ---------
-        org_row: dict[any]
-            The organisation to process, a list of org details (org_id, name)
-        user_rows: dict[any]
+        org_row: dict[str, any]
+            The organisation to process, a dictionary of org details (org_id, name).
+        user_rows: list[dict[str, any]]
             The users to process, a list of user details (user_id, name, email, token,
-            refresh_token)
-        user_auth_rows: dict[any]
+            refresh_token).
+        user_auth_rows: list[dict[str, any]]
             The user auth rows for all users, a list of user auth details (user_id, auth_key,
-            auth_value)
-        job: job
-            The job object for the current task
+            auth_value).
+        job: job.Job | None
+            The job object for the current task.
 
         Returns
         -------
-        :return: None
+        list[dict[str, any]]
+            A list of dictionaries containing the results of indexing each user.
         """
         logging.debug(f"Indexing org: {org_row}")
         logging.debug(f"Users: {user_rows}")
@@ -86,13 +96,13 @@ class Indexer:
             job.save_meta()
 
             logging.info("Task ID: %s, Message: %s", job.id, job.meta["status"])
-            logging.info("Indexing %s: %s", self.get_indexer_name, job.id)
+            logging.info("Indexing %s: %s", self.get_indexer_name(), job.id)
 
         result = []
         for user_row in user_rows:
             if job:
                 job.meta["status"] = (
-                    f"Indexing {self.get_indexer_name} for user {user_row['email']}"
+                    f"Indexing {self.get_indexer_name()} for user {user_row['email']}"
                 )
                 job.meta["org"] = org_row["name"]
                 job.meta["user"] = user_row["email"]
@@ -112,14 +122,14 @@ class Indexer:
             )
 
             logging.info(
-                f"User {user_row['email']} indexing \
-                    {'succeeded' if success else 'failed'}: {message}"
+                f"User {user_row['email']} indexing {'succeeded' if success else 'failed'}: \
+                    {message}"
             )
 
             if job:
                 job.meta[
                     "status"
-                ] = f"Indexing {self.get_indexer_name} for user {user_row['email']}: \
+                ] = f"Indexing {self.get_indexer_name()} for user {user_row['email']}: \
                         {'succeeded' if success else 'failed'}"
                 job.meta["org"] = org_row["name"]
                 job.meta["user"] = user_row["email"]
@@ -140,19 +150,31 @@ class Indexer:
         return result
 
     def index_user(
-        self: None,
-        user_row: dict[any],
-        org_row: dict[any],
-        user_auth_rows: dict[any],
-        job: job | None,
+        self,
+        user_row: dict[str, any],
+        org_row: dict[str, any],
+        user_auth_rows: list[dict[str, any]],
+        job: job.Job | None,
     ) -> tuple[bool, str]:
         """Process the Google Drive documents for a user and index them in Pinecone.
 
-        :param user: the user to process, a list of user details (user_id, name, email, token,
-            refresh_token)
-        :param org: the organisation to process, a list of org details (org_id, name)
+        Arguments
+        ---------
+        user_row: dict[str, any]
+            The user to process, a dictionary of user details (user_id, name, email, token,
+            refresh_token).
+        org_row: dict[str, any]
+            The organisation to process, a dictionary of org details (org_id, name).
+        user_auth_rows: list[dict[str, any]]
+            The user auth rows for the user, a list of user auth details (user_id, auth_key,
+            auth_value).
+        job: job.Job | None
+            The job object for the current task.
 
-        :return: bool
+        Returns
+        -------
+        Tuple[bool, str]
+            A tuple containing a success flag and a message.
         """
         raise NotImplementedError
 
@@ -160,27 +182,40 @@ class Indexer:
 class GoogleDriveIndexer(Indexer):
     """Used to process the Google Drive documents and index them in Pinecone."""
 
-    def __init__(self: None):
+    def __init__(self):
         super().__init__()
         self.google_creds = lorelai.utils.load_config("google")
 
     def index_user(
-        self: None,
-        user_row: dict[any],
-        org_row: dict[any],
-        user_auth_rows: dict[any],
-        job: job | None,
+        self,
+        user_row: dict[str, any],
+        org_row: dict[str, any],
+        user_auth_rows: list[dict[str, any]],
+        job: job.Job | None,
         folder_id: str = "",
     ) -> bool:
         """Process the Google Drive documents for a user and index them in Pinecone.
 
-        :param user: the user to process, a list of user details (user_id, name, email, token,
-            refresh_token)
-        :param org: the organisation to process, a list of org details (org_id, name)
+        Arguments
+        ---------
+        user_row: dict[str, any]
+            The user to process, a dictionary of user details (user_id, name, email, token,
+            efresh_token).
+        org_row: dict[str, any]
+            The organisation to process, a dictionary of org details (org_id, name).
+        user_auth_rows: list[dict[str, any]]
+            The user auth rows for the user, a list of user auth details (user_id, auth_key,
+            auth_value).
+        job: job.Job | None
+            The job object for the current task.
+        folder_id: str
+            The folder ID to process.
 
-        :return: bool
+        Returns
+        -------
+        bool
+            True if indexing was successful, False otherwise.
         """
-        # 1. Load the Google Drive credentials
         if user_row:
             logging.debug(f"Processing user: {user_row['email']} from org: {org_row['name']}")
             if folder_id:
@@ -188,13 +223,14 @@ class GoogleDriveIndexer(Indexer):
             else:
                 logging.debug("Processing all folders")
 
-            # get the user's refresh token by searhcing through the user_auth_rows where
-            # key = 'refresh_token' and user_id = user_row['user_id']
-            refresh_token = None
-            for user_auth_row in user_auth_rows:
-                if user_auth_row["auth_key"] == "refresh_token":
-                    refresh_token = user_auth_row["auth_value"]
-                    break
+            refresh_token = next(
+                (
+                    user_auth_row["auth_value"]
+                    for user_auth_row in user_auth_rows
+                    if user_auth_row["auth_key"] == "refresh_token"
+                ),
+                None,
+            )
 
             if not refresh_token:
                 msg = f"User {user_row['email']} does not have a refresh token"
@@ -217,7 +253,7 @@ class GoogleDriveIndexer(Indexer):
 
         # 2. Get the Google Drive document IDs
         logging.debug(f"Getting Google Drive document IDs for user: {user_row['email']}")
-        document_ids = self.get_google_docs_ids(credentials, folder_id)
+        document_ids = self.__get_google_docs_ids(credentials, folder_id)
 
         # 3. Generate the index name we will use in Pinecone
         index_name = lorelai.utils.pinecone_index_name(
@@ -253,28 +289,33 @@ class GoogleDriveIndexer(Indexer):
         logging.debug("Index statistics before indexing vs after indexing:")
         lorelai.utils.print_index_stats_diff(index_stats_before, index_stats_after)
 
-    def __get_google_docs_ids(self, credentials, folder_id: str = "") -> list[str]:
+    def __get_google_docs_ids(self, credentials: Credentials, folder_id: str = "") -> list[str]:
         """Retrieve all Google Docs document IDs from the user's Google Drive or a specific folder.
 
-        Note: this only includes Google Docs documents, not text files or pdfs.
+        Note: This only includes Google Docs documents, not text files or PDFs.
 
-        :param credentials: Google-auth credentials object for the user
-        :param folder_id: Optional Google Drive folder ID to restrict the search
-        :return: List of document IDs
+        Arguments
+        ---------
+        credentials: Credentials
+            Google-auth credentials object for the user.
+        folder_id: str
+            Optional Google Drive folder ID to restrict the search.
+
+        Returns
+        -------
+        list[str]
+            list of document IDs.
         """
         # Build the Drive v3 API service object
         service = build("drive", "v3", credentials=credentials)
 
         # List to store all document IDs
         document_ids = []
-
-        # Construct the query to filter files by mimeType and optional folder_id
-        if folder_id != "":
-            logging.debug(f"Getting Google Docs from folder: {folder_id}")
-            query = f"mimeType='application/vnd.google-apps.document' and '{folder_id}' in parents"
-        else:
-            logging.debug("Getting all Google Docs")
-            query = "mimeType='application/vnd.google-apps.document'"
+        query = (
+            f"mimeType='application/vnd.google-apps.document' and '{folder_id}' in parents"
+            if folder_id
+            else "mimeType='application/vnd.google-apps.document'"
+        )
 
         # Call the Drive v3 API to get the list of files. We don't use GoogleDriveLoader because
         # we only need the document IDs here.
@@ -304,7 +345,7 @@ class GoogleDriveIndexer(Indexer):
 
             logging.debug(f"Found {len(items)} files")
             for item in items:
-                logging.debug(f"Found file: {item['name']} with ID: {item['id']} ")
+                logging.debug(f"Found file: {item['name']} with ID: {item['id']}")
                 document_ids.append(item["id"])
 
             # Check if there are more pages
