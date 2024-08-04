f612c65e03fe8c3f95aa7b610752570736529ffa
Walter Heck <walterheck@helixiora.com>
2024-08-04 16:32:37 +0200

    change google login to go straight to backend route
    By passing a login-url to the google login code, we can have a callback directly to the backend, removing the need for some cumbersome javascript and passing back and firth json.
    
    Also, this commit improves the checking of the google creds

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -20,8 +20,8 @@ from flask import Blueprint, flash, g, jsonify, redirect, render_template, reque
 from google.auth import exceptions
 from google.auth.transport import requests
 from google.oauth2 import id_token
+import time
 
-from app.routes.google.auth import google_auth_url
 from app.utils import (
     get_db_connection,
     get_org_id_by_organisation,
@@ -79,7 +79,6 @@ def profile():
             google_api_key=google_api_key,
             google_docs_to_index=google_docs_to_index,
             features=g.features,
-            google_auth_url=google_auth_url(),
         )
     return "You are not logged in!", 403
 
@@ -284,54 +283,41 @@ def login():
         JSON: A JSON object with a message indicating whether the user was authenticated
         successfully.
     """
+    # get the received token from the JSON data that came from the web client
+    id_token_received = request.form.get("credential")
+    csrf_token = request.form.get("g_csrf_token")
+
+    # use this token to verify the user's identity
+    idinfo = id_token.verify_oauth2_token(id_token_received, requests.Request())
+
+    if not idinfo:
+        raise exceptions.GoogleAuthError("Invalid token")
+
+    # this function will raise an exception if the token is invalid
+    validate_id_token(idinfo, csrf_token=csrf_token)
+
+    # get some values from the info we got from google
+    logging.info("Info from token: %s", idinfo)
+    user_email = idinfo["email"]
+    username = idinfo["name"]
+    user_full_name = idinfo["name"]
+    google_id = idinfo["sub"]
+
+    # check if the user is already registered
+    user_id = get_user_id_by_email(user_email)
+
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+
     try:
-        if request.content_type != "application/json":
-            return jsonify({"message": "Invalid content type: " + request.content_type}), 400
-
-        data = request.get_json()
-        logging.info("Received JSON data: %s", data)
-
-        # get the received token from the JSON data that came from the web client
-        id_token_received = data.get("credential")
-
-        # use this token to verify the user's identity
-        idinfo = id_token.verify_oauth2_token(id_token_received, requests.Request())
-
-        if not idinfo:
-            raise exceptions.GoogleAuthError("Invalid token")
-
-        # this function will raise an exception if the token is invalid
-        validate_id_token(idinfo)
-
-        conn = get_db_connection()
-        cursor = conn.cursor(dictionary=True)
-
-        # get some values from the info we got from google
-        logging.info("Info from token: %s", idinfo)
-        user_email = idinfo["email"]
-        username = idinfo["name"]
-        user_full_name = idinfo["name"]
-        google_id = idinfo["sub"]
-
-        # check if the user is already registered
-        user_id = get_user_id_by_email(user_email)
-
         org_id = get_org_id_by_userid(cursor, user_id)
         organisation = get_organisation_by_org_id(cursor, org_id)
 
         # if we can't find the user, it means they are not registered
-        # we don't redirect directly, but send a message back to the frontend to redirect using JS
+        # redirect them to the registration page
         if not user_id:
-            return jsonify(
-                {
-                    "message": "Invalid login",
-                    "email": user_email,
-                    "full_name": user_full_name,
-                    "google_id": google_id,
-                    "google_token": id_token_received,
-                    "redirect_url": url_for("auth.register_get"),
-                }
-            ), 200
+            logging.info("User not registered: %s", user_email)
+            return redirect(url_for("auth.register_get", email=user_email, full_name=username))
 
         # if we're still here, it means we found the user, so we log them in
         logging.info("User logging in: %s", user_email)
@@ -347,7 +333,7 @@ def login():
 
         logging.debug("Session: %s", session)
 
-        return jsonify({"message": "User authenticated successfully", "redirect_url": "/"}), 200
+        return redirect(url_for("index"))
 
     except ValueError as e:
         logging.error("Invalid token: %s", e)
@@ -444,7 +430,7 @@ def is_username_available(username: str) -> bool:
     return not user
 
 
-def validate_id_token(idinfo: dict):
+def validate_id_token(idinfo: dict, csrf_token: str):
     """
     Validate the ID token.
 
@@ -452,12 +438,34 @@ def validate_id_token(idinfo: dict):
     ----------
     idinfo : dict
         The ID token information.
+    csrf_token : str
+        The CSRF token from the body of the POST request.
 
     Raises
     ------
     ValueError
         If the issuer is wrong or the email is not verified.
     """
+    # TODO: Add more checks here, see
+    # https://developers.google.com/identity/gsi/web/guides/verify-google-id-token
+
+    if not csrf_token:
+        raise exceptions.GoogleAuthError("No CSRF token in post body.")
+
+    csrf_token_cookie = request.cookies.get("g_csrf_token")
+    if not csrf_token_cookie:
+        raise exceptions.GoogleAuthError("No CSRF token in cookie.")
+
+    if csrf_token != csrf_token_cookie:
+        raise exceptions.GoogleAuthError("CSRF token mismatch.")
+
+    googlesettings = load_config("google")
+    if idinfo["aud"] not in googlesettings["client_id"]:
+        raise exceptions.GoogleAuthError("Wrong client ID.")
+
+    if idinfo["exp"] < time.time():
+        raise exceptions.GoogleAuthError("Token expired.")
+
     if idinfo["iss"] not in ["accounts.google.com", "https://accounts.google.com"]:
         raise exceptions.GoogleAuthError("Wrong issuer.")
     if not idinfo.get("email_verified"):
