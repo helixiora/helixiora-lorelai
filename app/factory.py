bf0457341f9156f7fa87c45499661028141bb4dc
Walter Heck <walterheck@helixiora.com>
2024-10-28 10:02:51 +0100

    HOTFIX: make sure the Sentry config is set up for the frontend to track the environment properly

--- app/factory.py
+++ app/factory.py
@@ -60,6 +60,7 @@ def create_app(config_name: str = "default") -> Flask:
             traces_sample_rate=1.0,
             profiles_sample_rate=1.0,
         )
+        logging.info("Sentry initialized in environment %s", app.config["SENTRY_ENVIRONMENT"])
 
     # Apply ProxyFix
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
