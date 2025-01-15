8a225d9bb357dadee3892e5a1b153261a018ea66
Masood-Helix <masood@helixiora.com>
2025-01-15 17:24:37 +0530

    fix init and seed db bugs

--- app/cli.py
+++ app/cli.py
@@ -2,7 +2,9 @@
 
 import click
 from flask.cli import with_appcontext
-from app.models import db, Datasource, Plan, Role
+from sqlalchemy import text
+
+from app.models import Datasource, Plan, Role, db
 
 
 @click.command("init-db")
@@ -12,7 +14,7 @@ def init_db_command():
     db.create_all()
 
     # create the alembic version table
-    db.session.execute("CREATE TABLE alembic_version (version_num VARCHAR(32) NOT NULL)")
+    db.session.execute(text("CREATE TABLE alembic_version (version_num VARCHAR(32) NOT NULL)"))
     db.session.commit()
 
     click.echo("Initialized the database.")
@@ -26,25 +28,31 @@ def seed_db_command():
 
     click.echo("Creating datasources...")
     # Add your seed data here, for example:
-    datasource = Datasource.create(name="Slack", type="oauth")
+    datasource = Datasource(datasource_name="Slack", datasource_type="oauth")
     db.session.add(datasource)
-    datasource = Datasource.create(name="Google Drive", type="oauth")
+    datasource = Datasource(datasource_name="Google Drive", datasource_type="oauth")
     db.session.add(datasource)
     db.session.commit()
 
     click.echo("Creating plans...")
-    plan = Plan.create(name="Free", price=0, duration=30, message_limit_daily=1000)
+    plan = Plan(plan_name="Free", price=0, duration_months=30, message_limit_daily=1000)
     db.session.add(plan)
-    plan = Plan.create(name="Pro", price=10, duration=30, message_limit_daily=10000)
+    plan = Plan(plan_name="Pro", price=10, duration_months=30, message_limit_daily=10000)
     db.session.add(plan)
     db.session.commit()
 
     click.echo("Creating roles...")
-    role = Role.create(name="org_admin", description="Admin role")
+    role = Role(
+        name="org_admin",
+    )
     db.session.add(role)
-    role = Role.create(name="super_admin", description="Super admin role")
+    role = Role(
+        name="super_admin",
+    )
     db.session.add(role)
-    role = Role.create(name="user", description="User role")
+    role = Role(
+        name="user",
+    )
     db.session.add(role)
     db.session.commit()
 
